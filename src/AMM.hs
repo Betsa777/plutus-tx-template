f9ab810cb6a764d2e9a7808c451c6396ee7e5811
Betsa777 <dakuyobetsaleel48@gmail.com>
2025-11-10 16:10:09 +0000

    Update with poolParams

--- src/AMM.hs
+++ src/AMM.hs
@@ -14,8 +14,6 @@
 
 module AMM where
 
--- import Plutus.V1.Ledger.Api ()
-
 import GHC.Generics (Generic)
 import PlutusLedgerApi.V1
   ( CurrencySymbol (..),
@@ -56,12 +54,28 @@ type AdaReserve = Integer
 type TokenReserve = Integer
 
 type LPTokenCurrencySymbol = CurrencySymbol
-
+type TokenSymbol = CurrencySymbol
 type LPTokenName = TokenName
 
+data PoolParams = PoolParams {
+    adaR:: AdaReserve,
+    tokR:: TokenReserve,
+    tokSymbol::TokenSymbol,
+    lpTokNumb:: LPTokenNumber,
+    lpPKH::  PubKeyHash,
+    lpCurrSymb:: LPTokenCurrencySymbol,
+    lpTokName:: LPTokenName
+   } deriving stock (Generic)
+     deriving anyclass (HasBlueprintDefinition)
+
+PlutusTx.makeIsDataSchemaIndexed ''PoolParams [('PoolParams,0)]
+--Redeemer have 3 parts:
+-- 1. AddLiquitdity
+-- 2. Swap a token to ada or swap a ada to token
+-- 3. Remove the liquidity
 data AMMRedeemer
-  = AddLiquidity Amount Amount AdaReserve TokenReserve LPTokenNumber LPPKH LPTokenCurrencySymbol LPTokenName
-  | Swap Amount LPPKH
+  = AddLiquidity Amount Amount PoolParams
+  | Swap Amount CurrencySymbol LPPKH
   | RemoveLiquidity Amount LPPKH
   deriving stock (Generic)
   deriving anyclass (HasBlueprintDefinition)
@@ -77,35 +91,41 @@ PlutusTx.makeIsDataSchemaIndexed
 ammValidator :: AMMRedeemer -> ScriptContext -> Bool
 ammValidator r ctx =
   case r of
-    AddLiquidity adaAmount tokenAmount adaR tokR lpTokNumb lpPKH lpCurrSymb lpTokName ->
-      P.traceIfFalse "AddLiquidty fails" (addLiquidity adaAmount tokenAmount adaR tokR lpTokNumb lpPKH lpCurrSymb lpTokName ctx)
-
+    AddLiquidity adaAmount tokenAmount poolParams ->
+      P.traceIfFalse "AddLiquidty fails" (addLiquidity adaAmount tokenAmount poolParams ctx)
+    -- Swap amount tokSymbol lpPKH ->
+    --   P.traceIfFalse "Swap fails" (swap amount tokSymbol lpPKH ctx)
 -- Swap tA tB amount owner ->
 --   P.traceIfFalse "Swap fails" (swap tA tB amount owner ctx)
 -- RemoveLiquidity tA tB amount owner ->
 --   P.traceIfFalse "Removal fails" (removeLiquidity tA tB amount owner ctx)
 
--- ajout de liquidité
 
+-- ajout de liquidité
 {-# INLINEABLE addLiquidity #-}
-addLiquidity :: Amount -> Amount -> AdaReserve -> TokenReserve -> LPTokenNumber -> LPPKH -> LPTokenCurrencySymbol -> LPTokenName -> ScriptContext -> Bool
-addLiquidity adaAmount tokenAmount adaR tokR lpTokNumb lpPKH lpCurrSymb lpTokName ctx =
+addLiquidity :: Amount -> Amount -> PoolParams -> ScriptContext -> Bool
+addLiquidity adaAmount tokenAmount poolParams ctx =
   if adaAmount <= 0 || tokenAmount <= 0
     then False
     else
       -- au cas ou il n'ya pas de reserve du token ajouté
-      if tokR == 0
+      if (tokR poolParams) == 0
         then
-          let tokensForLp = adaR + adaAmount
-           in finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTokName ctx
+          let tokensForLp = (adaR poolParams) P.+ adaAmount
+           in finalizeAdding adaAmount tokenAmount tokensForLp poolParams ctx
         else
-          let tokensForLp = P.divide (lpTokNumb P.* adaAmount) adaR
-           in finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTokName ctx
+          let tokensForLp = P.divide ((lpTokNumb poolParams) P.* adaAmount) (adaR poolParams)
+           in finalizeAdding adaAmount tokenAmount tokensForLp poolParams ctx
 
 {-# INLINEABLE finalizeAdding #-}
-finalizeAdding :: Amount -> Amount -> AdaReserve -> TokenReserve -> LPTokenNumber -> LPPKH -> LPTokenCurrencySymbol -> LPTokenName -> ScriptContext -> Bool
-finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTokName ctx =
-  List.and [goodReserves adaR tokR ctx, lpInput ctx, scriptOuput adaR tokR ctx, lpOutput ctx]
+finalizeAdding :: Amount -> Amount -> LPTokenNumber -> PoolParams -> ScriptContext -> Bool
+finalizeAdding adaAmount tokenAmount tokensForLp poolParams ctx =
+  List.and [
+      goodReserves (adaR poolParams) (tokR poolParams) ctx,
+      lpInput (tokSymbol poolParams) ctx,
+      scriptOuput (adaR poolParams) (tokR poolParams) (tokSymbol poolParams) ctx,
+      lpOutput poolParams ctx
+   ]
   where
     -- Je verifie les reserves en m'assurant que les reserves précisées correspondent
     -- bien a ce qui avait été hachés et sotckées dans le datumHash du pool
@@ -124,8 +144,8 @@ finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTo
             inputs
 
     -- inputs
-    lpInput :: ScriptContext -> Bool
-    lpInput ctx =
+    lpInput :: LPTokenCurrencySymbol -> ScriptContext -> Bool
+    lpInput tokSymb ctx =
       let inputs = txInfoInputs $ scriptContextTxInfo ctx
           -- verifier que le lp a le montant d'Ada qu'il fournit
           goodAdaInput =
@@ -141,7 +161,7 @@ finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTo
             List.any
               ( \inp ->
                   List.any
-                    (\(cs, _, am) -> (cs P./= adaSymbol) P.&& (am P.== tokenAmount))
+                    (\(cs, _, am) -> (cs P.== tokSymb) P.&& (am P.== tokenAmount))
                     (flattenValue $ txOutValue $ txInInfoResolved inp)
               )
               inputs
@@ -150,13 +170,13 @@ finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTo
     -- Outputs
     -- verifier que l'UTXO du script a été bien mis a jour
     -- avec le montant d'Ada et de token ajoutés aux reserves deja existantes
-    scriptOuput :: AdaReserve -> TokenReserve -> ScriptContext -> Bool
-    scriptOuput adaR tokR ctx =
+    scriptOuput :: AdaReserve -> TokenReserve -> LPTokenCurrencySymbol -> ScriptContext -> Bool
+    scriptOuput adaR tokR tokSymb ctx =
       case getContinuingOutputs ctx of
         [o] ->
           let values = flattenValue (txOutValue o)
               trueAdaAmount = List.any (\(cs, _, am) -> (cs P.== adaSymbol) P.&& (am P.== adaR P.+ adaAmount)) values
-              trueTokenAMount = List.any (\(cs, _, am) -> (cs P./= adaSymbol) P.&& (am P.== tokR P.+ tokenAmount)) values
+              trueTokenAMount = List.any (\(cs, _, am) -> (cs P.== tokSymb) P.&& (am P.== tokR P.+ tokenAmount)) values
               newDatumHash = getHash (adaAmount P.+ adaR) (tokenAmount P.+ tokR)
               goodDatumHash = case txOutDatum o of
                 OutputDatumHash (DatumHash hash) -> newDatumHash == hash
@@ -165,14 +185,14 @@ finalizeAdding adaAmount tokenAmount adaR tokR tokensForLp lpPKH lpCurrSymb lpTo
         -- lpOuput correspond à l'output correspondant
         -- a celui dans lequel le LP a recu les LP tokens ou tokensForLp
         _ -> False
-    lpOutput :: ScriptContext -> Bool
-    lpOutput ctx =
+    lpOutput :: PoolParams -> ScriptContext -> Bool
+    lpOutput poolParams ctx =
       List.any
         ( \o ->
-            valueOf (txOutValue o) lpCurrSymb lpTokName
+            valueOf (txOutValue o) (lpCurrSymb poolParams) (lpTokName poolParams)
               == tokensForLp
               P.&& ( case addressCredential (txOutAddress o) of
-                       PubKeyCredential ownerPKH -> ownerPKH == lpPKH
+                       PubKeyCredential ownerPKH -> ownerPKH == (lpPKH poolParams)
                        _ -> False
                    )
         )
@@ -186,6 +206,10 @@ getHash adaR tokR =
       finalHash = Builtins.sha2_256 $ Builtins.appendByteString adaHash tokHash
    in finalHash
 
+--swap part
+{-# INLINEABLE swap #-}
+swap:: Amount -> LPTokenCurrencySymbol -> LPPKH -> ScriptContext -> Bool
+swap amount tokSymbol lpPKH ctx = True
 ammUntypedValidator :: BuiltinData -> BuiltinData -> BuiltinData -> P.BuiltinUnit
 ammUntypedValidator _ redeemer ctx =
   -- check retourne ()
